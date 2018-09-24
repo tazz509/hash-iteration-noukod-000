4253671a285c73179d44d9a8ca9d5f83b4a8f053
carly <carly@noukod.com>
2018-09-24 16:23:33 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,7 +5,7 @@
 # suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
-winner = ""
+winner = " "
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
