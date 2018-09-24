73f10e9435d5f1035f7b84172fe243502191f74e
carly <carly@noukod.com>
2018-09-24 16:18:37 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,5 +6,5 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids.each {|name,age| puts "Happy Birthday #{name}! You are now #{age}years old"}
+  birthday_kids.each {|name,age| puts "Happy Birthday #{name}! You are now #{age}years old!"}
 end
